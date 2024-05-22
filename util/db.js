0062397e53cb4698f1c6ddc9080acad33445d410
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-05-22 14:07:07 +0530

    update

--- util/db.js
+++ util/db.js
@@ -1,7 +1,9 @@
 //setup and connect mongoose
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.DATABASE_URI)
+  .connect(
+    `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}`,
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
