cbeb1aed9fb2b7d6c9d9c2dd455a4a8c35529d83
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-06-06 15:27:31 +0530

    update

--- util/db.js
+++ util/db.js
@@ -4,7 +4,11 @@ mongoose
   .connect(
     `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}`,
   )
-  .then(() => console.log(`Connected to MongoDB at ${process.env.DATABASE_URL}`))
+  .then(() =>
+    console.log(
+      `Connected to MongoDB at mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}`,
+    ),
+  )
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 module.exports = mongoose;
